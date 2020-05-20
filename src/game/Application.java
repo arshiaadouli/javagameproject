1ca491a3b2267f696179ba48163b84e47801ed40
Arshia Adouli <aado0001@student.monash.edu>
2020-05-20 19:54:21 +1000

    problem with eatfood for player is fixed

--- src/game/Application.java
+++ src/game/Application.java
@@ -69,12 +69,12 @@ public class Application {
 		gameMap.at(42, 15).addItem(new Plank());
 		
 		// FIXME: Add more zombies!
-		gameMap.at(74, 20).addActor(new Zombie("Groan"));
-		gameMap.at(30,  18).addActor(new Zombie("Boo"));
-		gameMap.at(10,  4).addActor(new Zombie("Uuuurgh"));
-		gameMap.at(50, 18).addActor(new Zombie("Mortalis"));
-		gameMap.at(1, 10).addActor(new Zombie("Gaaaah"));
-		gameMap.at(62, 12).addActor(new Zombie("Aaargh"));
+//		gameMap.at(74, 20).addActor(new Zombie("Groan"));
+//		gameMap.at(30,  18).addActor(new Zombie("Boo"));
+//		gameMap.at(10,  4).addActor(new Zombie("Uuuurgh"));
+//		gameMap.at(50, 18).addActor(new Zombie("Mortalis"));
+//		gameMap.at(1, 10).addActor(new Zombie("Gaaaah"));
+//		gameMap.at(62, 12).addActor(new Zombie("Aaargh"));
 		
 		// testing code for Farmer class
 		
