64860ef74a2ed42fe485b5a00528128a13de80e0
jxyyu <jyuu0052@student.monash.edu>
2020-06-06 12:07:37 +1000

    Code update

--- src/game/SniperAimAction.java
+++ src/game/SniperAimAction.java
@@ -1,8 +1,14 @@
 package game;
 
+import java.util.ArrayList;
+
 import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.ActorLocations;
+import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Menu;
 
 public class SniperAimAction extends Action {
 	private SniperRifle sniper;
@@ -13,8 +19,21 @@ public class SniperAimAction extends Action {
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
-//		sniper.incAim();
-		return actor + " is aiming";
+		sniper.incAim();
+		Menu menu = new Menu();
+		Display display = new Display();
+		Actions actions = new Actions();
+		ActorLocations actorLocations = new ActorLocations();
+		ArrayList<Actor> targetList = new ArrayList<>();
+		
+		targetList.add(actorLocations.iterator().next());
+		
+		for (Actor a : targetList) {
+			actions.add(new SniperAimMenuAction(a));
+		}
+		
+		
+		return menu.showMenu(actor, actions, display).execute(actor, map);
 	}
 
 	@Override
