6829aafe69d4577d595df1c40dd6c9909880e630
Arshia Adouli <aado0001@student.monash.edu>
2020-06-10 15:55:57 +1000

    die action javadocs has been completed

--- src/game/DieAction.java
+++ src/game/DieAction.java
@@ -4,7 +4,18 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * this class represents the suicide action of player in every turn(quit game)
+ */
 public class DieAction extends Action {
+    /**
+     * this method will remove the player from the map which
+     * it is on therefore the game should end because there are
+     * no player which the world processes it turn.
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
        map.removeActor(actor);
